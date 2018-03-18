b9b30c609bb518aa6980435268c1fb7868cb2989
haruto167 <ex7477tanabe@gmail.com>
2018-03-18 12:57:53 +0900

    first commit

---
+++ src/main/java/work/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,12 @@
+package work.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApplication.class, args);
+	}
+}
